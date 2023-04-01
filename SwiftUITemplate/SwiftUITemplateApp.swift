6a0617e37c50728c3cb040a33e9101f40d826452
Patryk Jastrzebski <patryk.jastrzebski@iteo.com>
2023-04-01 16:06:43 +0200

    Initial Commit

---
+++ SwiftUITemplate/SwiftUITemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITemplateApp.swift
+//  SwiftUITemplate
+//
+//  Created by Patryk Jastrzębski on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
